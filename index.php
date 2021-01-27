5d45c0cbd8b8cad66011af18a6f9cc341cb21187
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-01-27 13:09:59 +0100

    Exo-152-PHP

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
 // Premiere ligne
+$date = date("j/m/Y");
+echo $date;
+echo "<br>";
+
+$da = mktime(0, 0, 0, 12, 10, 2018);
+echo date("Y.m.d", $da);
+echo "<br>";
+
+$da = mktime(11,35,7);
+echo date("g:i:s", $da);
 
 
 
